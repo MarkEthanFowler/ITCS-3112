619fd918e2efc793d02525496b85be8478633f85
MarkEthanFowler <methanfowler@gmail.com>
2025-03-11 08:55:16 -0400

    Added Class files
    Added the file for the project

---
+++ GPACalculator/GPACalculator/Grade.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPACalculator
+{
+    internal class Grade
+    {
+        private int creditHours;
+        private String letterGrade;
+
+        public Grade() { }
+
+        public Grade(int creditHours, String letterGrade)
+        {
+            this.creditHours = creditHours;
+            this.letterGrade = letterGrade;
+        }
+
+        public int getCreditHours()
+        {
+            return creditHours;
+        }
+
+        public void setCreditHours(int creditHours)
+        {
+            this.creditHours = creditHours;
+        }
+
+        public String getLetterGrade()
+        {
+            return letterGrade;
+        }
+
+        public void setLetterGrade(String letterGrade)
+        {
+            this.letterGrade = letterGrade;
+        }
+    }
+}
